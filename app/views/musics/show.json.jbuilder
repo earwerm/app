ed4237ee49db8f9f02d2d89fe2b7b6df7830cbea
earwerm <michael@earwerm.com>
2013-07-06 11:40:02 +1000

    Initial Commit

---
+++ app/views/musics/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @music, :name, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App::Application.config.secret_key_base = 'a9b4781ebdbf6ae53ba0c10b25586a1ab0f0df78fbb8653dcc3cb5cbb79c0e86f9360d3ba6e9473383305689daacd86f91def5ac96a91da0862bcc2a433b5b07'
